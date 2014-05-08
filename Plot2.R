bda6165b5612aa6bfdc33642dd78c240d177cbda
anonymousaditya <anonymousaditya@users.noreply.github.com>
2014-05-08 12:31:13 +0530

    Delete Plot2.R

--- Plot2.R
+++
@@ -1,8 +0,0 @@
-data <- read.table("C:/Users/aditya.gupta/Desktop/exdata-data-household_power_consumption/household_power_consumption.txt",
-                   header = TRUE,sep = ";",na.strings = "?")
-data<- subset(data, Date %in% c('1/2/2007','2/2/2007'))
-data$x <- paste(data$Date,data$Time)
-data$x <- strptime(data$x,"%d/%m/%Y %H:%M:%S")
-png(file = "plot2.png", width = 480, height = 480, units = "px")
-with(data,plot(x,Global_active_power, type = "l", xlab = "",ylab = "Global Active Power (kilowatts)"))
-dev.off()
