8220c5c1f2cad83d936880821f713a791b640c64
anonymousaditya <anonymousaditya@users.noreply.github.com>
2014-05-08 12:32:31 +0530

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,11 @@
+data <- read.table("C:/Users/aditya.gupta/Desktop/exdata-data-household_power_consumption/household_power_consumption.txt",
+                   header = TRUE,sep = ";",na.strings = "?")
+data<- subset(data, Date %in% c('1/2/2007','2/2/2007'))
+data$x <- paste(data$Date,data$Time)
+data$x <- strptime(data$x,"%d/%m/%Y %H:%M:%S")
+png(file = "plot3.png", width = 480, height = 480, units = "px")
+plot(data$x,data$Sub_metering_1, type = "l", xlab = "",ylab = "energy sub metering")
+lines(data$x,data$Sub_metering_2, type = "l", xlab = "",ylab = "energy sub metering", col = "red")
+lines(data$x,data$Sub_metering_3, type = "l", xlab = "",ylab = "energy sub metering", col = "blue")
+legend("topright",lwd=1,col = c("black","red","blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+dev.off()
